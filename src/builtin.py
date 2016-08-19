b49ae6aacc4bc2f629f111634f88d62c02766c74
tim_zallin <tim.zallin@gmail.com>
2016-08-19 18:11:12 +0300

    module for builtin procs added

---
+++ src/builtin.py
@@ -0,0 +1,60 @@
+import operator as op
+import functools as ft
+
+# TO THINK
+# change to function with corresponding repr ?
+
+
+class EqualityOp:
+
+    @staticmethod
+    def __repr__():
+        return '='
+
+    @staticmethod
+    def apply(a, b):
+        return a == b
+
+
+class SumOp:
+
+    @staticmethod
+    def __repr__():
+        return '+'
+
+    @staticmethod
+    def apply(*args):
+        return ft.reduce(lambda v, el: v + el, args, 0)
+
+
+class MultOp:
+
+    @staticmethod
+    def __repr__():
+        return '*'
+
+    @staticmethod
+    def apply(*args):
+        return ft.reduce(lambda v, el: v * el, args, 1)
+
+
+class SubstractOp:
+
+    @staticmethod
+    def __repr__():
+        return '-'
+
+    @staticmethod
+    def apply(*args):
+        return ft.reduce(lambda v, el: v - el, args[1:], args[0])
+
+
+class GreaterOp:
+
+    @staticmethod
+    def __repr__():
+        return '>'
+
+    @staticmethod
+    def apply(a, b):
+        return a > b
