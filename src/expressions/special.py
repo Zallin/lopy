6c9582ee1e363feae5bec9868814c83f7d73c204
tim_zallin <tim.zallin@gmail.com>
2016-08-19 18:09:01 +0300

    special forms expressions evaluation logic added

--- src/expressions/special.py
+++ src/expressions/special.py
@@ -1,4 +1,6 @@
-from src.expressions.expression_abc import Expression
+import src.inspect_expr as ins
+
+from src.lopy_abc import Expression
 
 
 class Quote(Expression):
@@ -14,39 +16,40 @@ class Quote(Expression):
         return ['quote']
 
 
-class Assignment(Expression):
-
-    def __init__(self, name, expr):
-        pass
-
-    def eval(self, env):
-        pass
-
-    @staticmethod
-    def supported_exprs():
-        return ['set!']
-
-
 class Definition(Expression):
 
-    def __init__(self, name, expr):
-        pass
+    def __init__(self, var, *args):
+        self.name = var.name()
+        self.param_names = var.operands_names() if ins.is_application(var) else None
+        self.exprs = args
 
     def eval(self, env):
-        pass
+        if self.param_names:
+            lambda_obj = Lambda(self.param_names, *self.exprs)
+            expr_res = lambda_obj.eval(env)
+        else:
+            expr = self.exprs[0]
+            expr_res = expr.eval(env)
+        env.set_binding(self.name, expr_res)
 
     @staticmethod
     def supported_exprs():
-        return ['define']
+        return ['define', 'set!']
 
 
 class IfCondition(Expression):
 
     def __init__(self, pred, conseq, alt):
-        pass
+        self.pred = pred
+        self.conseq = conseq
+        self.alt = alt
 
     def eval(self, env):
-        pass
+        res = self.pred.eval(env)
+        if res:
+            return self.conseq.eval(env)
+        else:
+            return self.alt.eval(env)
 
     @staticmethod
     def supported_exprs():
@@ -55,24 +58,54 @@ class IfCondition(Expression):
 
 class Lambda(Expression):
 
-    def __init__(self, args, body):
-        pass
+    def __init__(self, args_obj, *args):
+        # TO FIX
+        # args_obj should be list of args, not application instance
+        if isinstance(args_obj, list):
+            self.args = args_obj
+        else:
+            self.args = [args_obj.name()]
+            self.args.extend(args_obj.operands_names())
+        self.body = args
 
     def eval(self, env):
-        pass
+        return RuntimeLambda(self.args, self.body, env)
 
     @staticmethod
     def supported_exprs():
         return ['lambda']
 
 
+class RuntimeLambda:
+
+    def __init__(self, args, body, env):
+        self.args = args
+        self.body = body
+        self.env = env
+
+    def eval(self, new_env):
+        return [expr.eval(new_env) for expr in self.body][-1]
+
+    def environment(self):
+        return self.env
+
+    def arg_list(self):
+        return self.args
+
+    # TO FIX
+    # move to different module
+    @staticmethod
+    def supported_exprs():
+        return []
+
+
 class BeginExpr(Expression):
 
     def __init__(self, *args):
-        pass
+        self.exprs = args
 
     def eval(self, env):
-        pass
+        return [expr.eval(env) for expr in self.exprs][-1]
 
     @staticmethod
     def supported_exprs():
@@ -82,10 +115,16 @@ class BeginExpr(Expression):
 class Cond(Expression):
 
     def __init__(self, *args):
-        pass
+        self.cond_seq = args
 
     def eval(self, env):
-        pass
+        for cond in self.cond_seq:
+            if cond.name() == 'else':
+                return cond.op_args[0].eval(env)
+            pred_res = cond.op.eval(env)
+            if pred_res:
+                return cond.op_args[0].eval(env)
+        return None
 
     @staticmethod
     def supported_exprs():
