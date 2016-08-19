bf9a0281f66109c710b439f36c98be262ad00353
tim_zallin <tim.zallin@gmail.com>
2016-08-19 18:09:49 +0300

    environment extend added

--- src/interpreter.py
+++ src/interpreter.py
@@ -1,21 +1,38 @@
-from parser import Parser
+import src.dynamic_import as di
+
+from src.parser import Parser
+
+
+class Environment:
+
+    def __init__(self, enclosing=None):
+        self.env = {}
+        self.enclosing = enclosing
+
+    def set_binding(self, name, obj):
+        self.env[name] = obj
+
+    def find(self, name):
+        if name in self.env:
+            return self.env[name]
+        if self.enclosing:
+            return self.enclosing.find(name)
+
+    def extend(self):
+        return type(self)(enclosing=self)
 
 
 class Interpreter:
 
     def __init__(self):
-        self.global_env = self._init_global_env()
+        self.global_env = Environment()
+        for Proc_class in di.import_module_classes('src.builtin'):
+            proc_obj = Proc_class()
+            self.global_env.set_binding(repr(proc_obj), proc_obj)
         self.parser = Parser()
 
     def eval(self, raw_src):
         exprs = self.parser.parse(raw_src)
         for exp in exprs:
-            res = self._eval(exp, self.global_env)  # ?
+            res = exp.eval(self.global_env)
         return res
-
-    @staticmethod
-    def _init_global_env():
-        pass
-
-    def _eval(self, exp, env):
-        pass
